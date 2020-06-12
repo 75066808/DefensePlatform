343a7e2fc70f0159305c4c97faa8a16b1dd525b0
75066808 <75066808@qq.com>
2020-06-12 22:05:45 +0800

    update medical help interface

--- src/main/java/example/controller/MessageController.java
+++ src/main/java/example/controller/MessageController.java
@@ -70,35 +70,6 @@ public class MessageController {
         return map;
     }
 
-    // {user=HLSSSS, pwd=123, province=Anhui, address=china, real-name=yuruijing, age=99, symptom=fever, color=grean}
-    // {user=HLSSSS, pwd=123, province=Anhui, address=, real-name=, age=, symptom=confirm, color=yellow}
-    // {user=HLSSSS, pwd=123, province=Anhui, address=, real-name=, age=, color=orange}
-    @RequestMapping(value="/report/send", method=RequestMethod.POST)
-    @ResponseBody
-    public void sendReport(@RequestBody Map<String, Object> report) {
-        System.out.println("sendReport");
-        System.out.println(report);
-    }
-
-    @RequestMapping(value="/report/find", method=RequestMethod.POST)
-    @ResponseBody
-    public Map<String, Object> findReport(String username, String function) {
-        System.out.println("findReport");
-        System.out.println(username);
-        Map<String, Object> map = new HashMap<>();
-        map.put("today_total_people", 1);
-        map.put("yesterday_total_people", 2);
-        map.put("today_reported", 3);
-        map.put("yesterday_reported", 4);
-        map.put("today_not_reported", 5);
-        map.put("yesterday_not_reported", 6);
-        map.put("today_normal", 7);
-        map.put("yesterday_normal", 8);
-        map.put("today_unnormal", 9);
-        map.put("yesterday_unnormal", 10);
-        return map;
-    }
-
     /*
     {user=HLSSSS, pwd=123, roles=Main, invite-code=}
     {user=HLSSSS, pwd=123, roles=Info, invite-code=}
@@ -387,4 +358,146 @@ public class MessageController {
         System.out.println(action);
     }
 
+    // {user=HLSSSS, pwd=123, province=Anhui, address=china, real-name=yuruijing, age=99, symptom=fever, color=grean}
+    // {user=HLSSSS, pwd=123, province=Anhui, address=, real-name=, age=, symptom=confirm, color=yellow}
+    // {user=HLSSSS, pwd=123, province=Anhui, address=, real-name=, age=, color=orange}
+    @RequestMapping(value="/report/send", method=RequestMethod.POST)
+    @ResponseBody
+    public void sendReport(@RequestBody Map<String, Object> report) {
+        System.out.println("sendReport");
+        System.out.println(report);
+    }
+
+    @RequestMapping(value="/report/find", method=RequestMethod.POST)
+    @ResponseBody
+    public Map<String, Object> findReport(String username, String function) {
+        System.out.println("findReport");
+        System.out.println(username);
+        Map<String, Object> map = new HashMap<>();
+        map.put("today_total_people", 1);
+        map.put("yesterday_total_people", 2);
+        map.put("today_reported", 3);
+        map.put("yesterday_reported", 4);
+        map.put("today_not_reported", 5);
+        map.put("yesterday_not_reported", 6);
+        map.put("today_normal", 7);
+        map.put("yesterday_normal", 8);
+        map.put("today_unnormal", 9);
+        map.put("yesterday_unnormal", 10);
+        return map;
+    }
+
+    // {user=HLSSSS, pwd=123, phonenumber=22222, province=Anhui, address=china, realname=urara, gender=male, age=99, department=1212, symptom=s}
+    @RequestMapping(value="/medical_help/send", method=RequestMethod.POST)
+    @ResponseBody
+    public void sendMedicalHelp(@RequestBody Map<String, Object> help) {
+        System.out.println("sendMedicalHelp");
+        System.out.println(help);
+    }
+
+
+    @RequestMapping(value="/medical_help/find/num", method=RequestMethod.POST)
+    @ResponseBody
+    public int findMedicalHelpNum(String username, String function) {
+        System.out.println("findMedicalHelpNum");
+        System.out.println(username);
+        return 5;
+    }
+    // {user=HLSSSS, pwd=123, phonenumber=22222, province=Anhui, address=china, realname=urara, gender=male, age=99, department=1212, symptom=s}
+
+    @RequestMapping(value="/medical_help/find/page", method=RequestMethod.POST)
+    @ResponseBody
+    public List<Map<String, Object>> findMedicalHelpPage(String username, String function, int page) {
+        System.out.println("findMedicalHelpPage");
+        System.out.println(username);
+        System.out.println(page);
+
+        List<Map<String, Object>> list = new LinkedList<>();
+        for (int i = 0;i < 10;i++) {
+            Map<String, Object> map = new HashMap<>();
+            map.put("sender", "zhang" + ((page - 1) * 10 + i));
+            map.put("department",  "D" + ((page - 1) * 10 + i));
+            list.add(map);
+        }
+        return list;
+    }
+
+    @RequestMapping(value="/medical_help/find/content", method=RequestMethod.POST)
+    @ResponseBody
+    public Map<String, Object> findMedicalHelpContent(String username, int page, int num) {
+        System.out.println("findMedicalHelpContent");
+
+        System.out.println(username);
+        System.out.println(page);
+        System.out.println(num);
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("user", "zhang" + ((page - 1) * 10 + num - 1));
+        map.put("pwd", "123");
+        map.put("phonenumber", "22222");
+        map.put("province", "Anhui");
+        map.put("address", "china");
+        map.put("realname", "urara");
+        map.put("gender", "male");
+        map.put("age", "99");
+        map.put("department", "1212");
+        map.put("symptom", "ssssssssssss");
+        return map;
+    }
+
+    // {user=zhang7, pwd=123, phonenumber=22222, province=Anhui, address=china, realname=urara, gender=male, age=99, department=1212, symptom=s, target-doctor=c}
+    @RequestMapping(value="/medical_help/distribute/send", method=RequestMethod.POST)
+    @ResponseBody
+    public void sendDistributeMedicalHelp(@RequestBody Map<String, Object> help) {
+        System.out.println("sendDistributeMedicalHelp");
+        System.out.println(help);
+    }
+
+    @RequestMapping(value="/medical_help/distribute/find/num", method=RequestMethod.POST)
+    @ResponseBody
+    public int findDistributeMedicalHelpNum(String username, String function) {
+        System.out.println("findMedicalHelpNum");
+        System.out.println(username);
+        return 5;
+    }
+
+    @RequestMapping(value="/medical_help/distribute/find/page", method=RequestMethod.POST)
+    @ResponseBody
+    public List<Map<String, Object>> findDistributeMedicalHelpPage(String username, String function, int page) {
+        System.out.println("findDistributeMedicalHelpPage");
+        System.out.println(username);
+        System.out.println(page);
+
+        List<Map<String, Object>> list = new LinkedList<>();
+        for (int i = 0;i < 10;i++) {
+            Map<String, Object> map = new HashMap<>();
+            map.put("sender", "zhang" + ((page - 1) * 10 + i));
+            map.put("department",  "D" + ((page - 1) * 10 + i));
+            list.add(map);
+        }
+        return list;
+    }
+
+    @RequestMapping(value="/medical_help/distribute/find/content", method=RequestMethod.POST)
+    @ResponseBody
+    public Map<String, Object> findDistributeMedicalHelpContent(String username, int page, int num) {
+        System.out.println("findDistributeMedicalHelpContent");
+
+        System.out.println(username);
+        System.out.println(page);
+        System.out.println(num);
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("user", "zhang" + ((page - 1) * 10 + num - 1));
+        map.put("pwd", "123");
+        map.put("phonenumber", "22222");
+        map.put("province", "Anhui");
+        map.put("address", "china");
+        map.put("realname", "urara");
+        map.put("gender", "male");
+        map.put("age", "99");
+        map.put("department", "1212");
+        map.put("symptom", "ssssssssssss");
+        return map;
+    }
 }
