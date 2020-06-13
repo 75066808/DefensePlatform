93bec52b5d94a3aec9a1a8dca6e06ccdb7429038
zhangyanzhes <stevenzhang1999@qq.com>
2020-06-13 11:09:05 +0800

    local test

--- src/main/java/example/service/MessageService.java
+++ src/main/java/example/service/MessageService.java
@@ -6,8 +6,10 @@ import example.dao.UserDao;
 import example.pojo.Message.Message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import example.pojo.Message.Msg.*;
 
 import java.util.List;
+import java.util.ArrayList;
 
 @Service
 public class MessageService {
@@ -18,43 +20,85 @@ public class MessageService {
     @Autowired
     private UserDao userDao;
 
-    public List<Message> findMessages(String id) {
-        return null;//messageDao.findMessages(id);
+    public void sendChat(Chat chat) {
+        messageDao.addchat(chat);
     }
 
-    public List<Message> findMessages(String to, String type) {
-        return null;//messageDao.findMessages(to, type);
+    public int findChatNum(String username) {
+        List<Chat> chatlist = messageDao.show_chat_to(username);
+        return chatlist.size()/10 + 1;
     }
 
-    public List<Message> findMessages(String from, String to, String type) {
-        return null;//messageDao.findMessages(from, to, type);
+    public List<Chat> findChatPage(String username, int page) {
+        List<Chat> chatlist = messageDao.show_chat_to(username);
+        List<Chat> newlist = new ArrayList<Chat>();
+        for (int i = (page - 1) * 10; i < chatlist.size() && i < page * 10; i++){
+            newlist.add(chatlist.get(i));
+        }
+        return newlist;
     }
 
-
-    public List<Message> fetchMessages(String type) {
-        return null;//messageDao.fetchMessages(type);
+    public Chat findChatContent(String username, int page, int num) {
+        List<Chat> chatlist = messageDao.show_chat_to(username);
+        return chatlist.get((page - 1) * 10 + num - 1);
     }
 
-
-    public boolean sendMessage(Message message) {
-        //messageDao.addMessage(message);
-        return true;
+    public void sendInviteCode(invite_code invite) {
+        messageDao.add_invite_code(invite);
     }
 
-    public boolean sendMessage(Message message, List<String> usernames) {
-        //messageDao.addMessage(message, usernames);
-        return true;
+    public void sendCommunityAdminApply(apply_community apply) {
+        messageDao.add_apply_community(apply);
     }
 
-
-    public boolean modifyMessage(String id, Message message) {
-        //messageDao.modifyMessage(message, id);
-        return true;
+    public void sendCommercialAdminApply(apply_commerical apply) {
+        messageDao.add_apply_commerical(apply);
     }
 
-    public boolean deleteMessage(String id) {
-        //messageDao.deleteMessage(id);
-        return true;
+    public void sendMedicalAdminApply(Apply_medical_admin apply) {
+        messageDao.add_apply_medical_admin(apply);
     }
 
+    public void sendInformationAdminApply(Apply_info apply) {
+        messageDao.add_apply_info(apply);
+    }
+
+    public void sendDoctorApply(apply_doctor apply) {
+        messageDao.add_apply_doctor(apply);
+    }
+
+    public void sendCitizenApply(Apply_citizen apply) {
+        messageDao.add_apply_citizen(apply);
+    }
+
+    public void sendMerchatApply(Apply_business apply) {
+        messageDao.add_apply_business(apply);
+    }
+
+    public void sendOpeningApply(Apply_opening apply) {
+        //messageDao.add_opening(apply);
+    }
+
+    public int findRoleApplyNum(String username, String type) {
+        if(type.equals("Main Admin application")){
+            List<Apply_main> applylist = messageDao.show_apply_main();
+            //check user name
+            return chatlist.size()/10 + 1;
+        }
+        else if(type.equals("Commercial Admin application")){
+
+        }
+        else if(type.equals("Info Admin application")){
+
+        }
+        else if(type.equals("Area Admin application")){
+
+        }
+        else if(type.equals("Doctor application")){
+
+        }
+        else if(type.equals("Medical Admin application")){
+
+        }
+    }
 }
