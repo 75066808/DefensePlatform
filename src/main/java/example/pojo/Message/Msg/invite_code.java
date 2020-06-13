7b0c5bc28767eb6ae3af6c103205bd1d8dee584c
75066808 <75066808@qq.com>
2020-06-13 16:42:16 +0800

    add chat and code convert

--- src/main/java/example/pojo/Message/Msg/invite_code.java
+++ src/main/java/example/pojo/Message/Msg/invite_code.java
@@ -1,5 +1,8 @@
 package example.pojo.Message.Msg;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class invite_code {
     public String code;
     public int type;
@@ -11,6 +14,33 @@ public class invite_code {
     ) {
         this.code = code;
         this.type = type;
+    }
+
+    /*
+    {user=HLSSSS, pwd=123, roles=Main, invite-code=}
+    {user=HLSSSS, pwd=123, roles=Info, invite-code=}
+    {user=HLSSSS, pwd=123, roles=Area, invite-code=}
+    {user=HLSSSS, pwd=123, roles=Commercial, invite-code=}
+    {user=HLSSSS, pwd=123, roles=Doctor, invite-code=}
+    {user=HLSSSS, pwd=123, roles=Medical, invite-code=}
+    */
+    static Map<String, Integer> typeMap = new HashMap<String, Integer>() {
+        {
+            put("Main", 0);
+            put("Info", 1);
+            put("Area", 2);
+            put("Commercial", 3);
+            put("Doctor", 4);
+            put("Medical", 5);
+
+        }
+    };
+
+    static invite_code convertMapToClass(Map<String, Object> map) {
+        return new invite_code(
+                (String)map.get("invite-code"),
+                typeMap.get((String)map.get("roles"))
+        );
     }
 
 }
